8320a1d921aa8d4f672c7353e81786349e32987f
Gurleen Sethi <sarusethi@rocketmail.com>
2020-07-12 17:44:44 -0400

    Add generic dialog.

--- script/script.js
+++ script/script.js
@@ -5,4 +5,37 @@ window.onload = () => {
   contactMeBtn.addEventListener("click", function (event) {
     contactSection.scrollIntoView({ behavior: "smooth" });
   });
+
+  [
+    { dialogId: "details-dialog-concordia", btnId: "view-details-concordia" },
+  ].forEach(({ btnId, dialogId }) => setUpDetailsDialog(btnId, dialogId));
 };
+
+function setUpDetailsDialog(viewDetailsBtnId, dialogId) {
+  const viewDetailsBtn = document.getElementById(viewDetailsBtnId);
+  const dialog = document.getElementById(dialogId);
+  const dialogContent = dialog.getElementsByClassName("dialog-content")[0];
+  const closeBtn = dialog.getElementsByClassName("close-btn")[0];
+
+  function openDialog() {
+    dialog.classList.add("show-dialog");
+  }
+
+  function closeDialog() {
+    dialogContent.classList.add("hide-dialog");
+    setTimeout(() => {
+      dialogContent.classList.remove("hide-dialog");
+      dialog.classList.remove("show-dialog");
+    }, 500);
+  }
+
+  viewDetailsBtn.addEventListener("click", openDialog);
+  dialogContent.addEventListener("click", (e) => e.stopPropagation());
+  dialog.addEventListener("click", closeDialog);
+  closeBtn.addEventListener("click", closeDialog);
+}
+
+function hideDialog(e) {
+  const element = document.getElementById("");
+  element.classList.remove("show-dialog");
+}
