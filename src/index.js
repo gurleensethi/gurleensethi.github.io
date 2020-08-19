439dd25ac464e2c7e2e8288630bd55dd60820016
Gurleen Sethi <sarusethi@rocketmail.com>
2020-08-19 19:07:53 -0400

    Update webpack.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ require("./css/styles.css");
 require("./css/styles-desktop.css");
 require("./css/styles-tablet.css");
 require("./images/blog_icon.png");
+require("./script/script");
 require.context("./images", true, /\.(png|jpg|svg)$/);
