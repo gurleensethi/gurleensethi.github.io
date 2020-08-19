547e9189f21c26ca0a181f38bd7ea49427c08a39
Gurleen Sethi <sarusethi@rocketmail.com>
2020-08-18 20:53:27 -0400

    Add webpack.

---
+++ src/index.js
@@ -0,0 +1,6 @@
+require("./css/common.css");
+require("./css/styles.css");
+require("./css/styles-desktop.css");
+require("./css/styles-tablet.css");
+require("./images/blog_icon.png");
+require.context("./images", true, /\.(png|jpg|svg)$/);
