07d12ad041282182da55648fb372be3d53dc801a
Gurleen Sethi <sarusethi@rocketmail.com>
2020-08-25 01:57:57 -0400

    Update webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,47 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { merge } = require("webpack-merge");
 
-module.exports = {
-  devServer: {
-    stats: "errors-only",
-    host: process.env.HOST,
-    port: process.env.PORT,
-    open: true,
-    overlay: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Gurleen Sethi",
-      template: "src/index.html",
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+module.exports = ({ mode }) => {
+  return merge(
+    {
+      mode,
+      devServer: {
+        stats: "errors-only",
+        host: process.env.HOST,
+        port: process.env.PORT,
+        open: true,
+        overlay: true,
       },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
+      plugins: [
+        new HtmlWebpackPlugin({
+          title: "Gurleen Sethi",
+          template: "src/index.html",
+        }),
+      ],
+      module: {
+        rules: [
           {
-            loader: "file-loader",
-            options: {
-              outputPath: "images/",
-              name: "[name].[ext]",
-            },
+            test: /\.css$/,
+            use: ["style-loader", "css-loader"],
           },
           {
-            loader: "img-loader",
+            test: /\.(png|svg|jpg|gif)$/,
+            use: [
+              {
+                loader: "file-loader",
+                options: {
+                  outputPath: "images/",
+                  name: "[name].[ext]",
+                },
+              },
+              {
+                loader: "img-loader",
+              },
+            ],
           },
         ],
       },
-    ],
-  },
+    },
+    require(`./webpack.config.${mode}`)
+  );
 };
