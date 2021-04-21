aacb46a3a357e0b7b1d3448ee4962aded67d5f47
Gurleen Sethi <sarusethi@rocketmail.com>
2021-04-21 11:40:07 -0400

    feat: delete all old files.

--- src/index.js
+++
@@ -1,7 +0,0 @@
-require("./css/common.css");
-require("./css/styles.css");
-require("./css/styles-desktop.css");
-require("./css/styles-tablet.css");
-require("./images/blog_icon.png");
-require("./script/script");
-require.context("./images", true, /\.(png|jpg|svg)$/);
