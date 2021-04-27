40af7dc38d8693118ab6b6e6a8330a6c2e70e408
Gurleen Sethi <sarusethi@rocketmail.com>
2021-04-26 23:50:04 -0400

    feat: add socials data.

--- website/components/UserInfo.tsx
+++ website/components/UserInfo.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { GeneralInfo } from "../types";
 import styles from "./UserInfo.module.css";
+import Image from "next/image";
 
 export interface UserInfoProps {
   info: GeneralInfo;
@@ -17,6 +18,26 @@ export const UserInfo: FC<UserInfoProps> = ({ info }) => {
         <h1 className="text-xl font-light flex text-center mb-8 max-w-screen-sm">
           {info.tagline}
         </h1>
+        <div className="mb-8">
+          {info.social.map((item) => {
+            return (
+              <a
+                key={item.name}
+                href={item.link}
+                target="_blank"
+                rel="noopener"
+                className="trasnition opacity-80 hover:opacity-100 mr-6 last:mr-0"
+              >
+                <Image
+                  src={item.icon}
+                  height="32px"
+                  width="32px"
+                  alt={item.name}
+                />
+              </a>
+            );
+          })}
+        </div>
         <button className="btn">Contact Me</button>
       </div>
     </>
