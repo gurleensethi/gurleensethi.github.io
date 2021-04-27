8d15db56db2f39432bfac69d607fa6d447ce0899
Gurleen Sethi <sarusethi@rocketmail.com>
2021-04-26 23:50:04 -0400

    feat: add socials data.

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -14,6 +14,7 @@ module.exports = {
   variants: {
     extend: {
       backgroundColor: ["active"],
+      margin: ["last"],
     },
   },
   plugins: [],
--- website/types.ts
+++ website/types.ts
@@ -5,6 +5,7 @@ export interface GeneralInfo {
   social: {
     name: string;
     link: string;
+    icon: string;
   }[];
 }
 
