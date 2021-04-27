492c54b93ec6f9c8e314658ddd09c04018a8d870
Gurleen Sethi <sarusethi@rocketmail.com>
2021-04-26 23:31:05 -0400

    feat: animation to rotate hand.

--- website/components/UserInfo.tsx
+++ website/components/UserInfo.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import { GeneralInfo } from "../types";
+import styles from "./UserInfo.module.css";
 
 export interface UserInfoProps {
   info: GeneralInfo;
@@ -7,13 +8,17 @@ export interface UserInfoProps {
 
 export const UserInfo: FC<UserInfoProps> = ({ info }) => {
   return (
-    <div className="p-8 h-screen flex flex-col justify-center items-center">
-      <h1 className="text-3xl mb-2 font-light">Hey 👋 I am</h1>
-      <h1 className="text-4xl font-medium mb-8">{info.name}</h1>
-      <h1 className="text-lg flex text-center mb-8">{info.tagline}</h1>
-      <button className="btn">
-        Contact Me
-      </button>
-    </div>
+    <>
+      <div className="p-8 min-h-screen flex flex-col justify-center items-center">
+        <h1 className="text-3xl mb-2 font-light">
+          Hey <span className={styles["hand-anim"]}>👋</span> I am
+        </h1>
+        <h1 className="text-4xl font-medium mb-8">{info.name}</h1>
+        <h1 className="text-xl font-light flex text-center mb-8 max-w-screen-sm">
+          {info.tagline}
+        </h1>
+        <button className="btn">Contact Me</button>
+      </div>
+    </>
   );
 };
